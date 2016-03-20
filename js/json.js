848c4f593cfcb264d1fba14c02ca5fa30283a44b
songyanzf <762350299@qq.com>
2016-03-20 17:41:53 +0800

    JS

--- js/json.js
+++ js/json.js
@@ -1,10 +1,10 @@
 var likeDate = [
-    {"imgLike": "../JD项目/images/likePic1.jpg", "title": "乔丹（qiaodan）百搭女板鞋 简约运动鞋平底鞋", "prace": "¥119.00"},
-    {"imgLike": "../JD项目/images/likePic2.jpg", "title": "德尔惠Deerway系带保暖男鞋高帮休闲板鞋潮", "prace": "¥109.00"},
-    {"imgLike": "../JD项目/images/likePic3.jpg", "title": "美好家 简易衣柜 布衣柜多 功能衣橱钢管107有侧布绿条", "prace": "¥69.00"},
-    {"imgLike": "../JD项目/images/likePic4.jpg", "title": "溢彩年华（紫色）时尚简易衣柜衣橱 布衣柜 DKB2-058", "prace": "¥49.00"},
-    {"imgLike": "../JD项目/images/likePic5.jpg", "title": "虎牌双保险保管柜/箱BGX-M/D-50Z办公家用四代豪华版高60cm特价", "prace": "¥999.00"},
-    {"imgLike": "../JD项目/images/likePic6.jpg", "title": "得力（deli）33120 至尊金贵系列办公家用保管箱 全钢防盗 震动报警 高36CM", "prace": "¥419.00"}
+    {"imgLike": "./images/likePic1.jpg", "title": "乔丹（qiaodan）百搭女板鞋 简约运动鞋平底鞋", "prace": "¥119.00"},
+    {"imgLike": "./images/likePic2.jpg", "title": "德尔惠Deerway系带保暖男鞋高帮休闲板鞋潮", "prace": "¥109.00"},
+    {"imgLike": "./images/likePic3.jpg", "title": "美好家 简易衣柜 布衣柜多 功能衣橱钢管107有侧布绿条", "prace": "¥69.00"},
+    {"imgLike": "./images/likePic4.jpg", "title": "溢彩年华（紫色）时尚简易衣柜衣橱 布衣柜 DKB2-058", "prace": "¥49.00"},
+    {"imgLike": "./images/likePic5.jpg", "title": "虎牌双保险保管柜/箱BGX-M/D-50Z办公家用四代豪华版高60cm特价", "prace": "¥999.00"},
+    {"imgLike": "./images/likePic6.jpg", "title": "得力（deli）33120 至尊金贵系列办公家用保管箱 全钢防盗 震动报警 高36CM", "prace": "¥419.00"}
 ],recomData=[
     {"recomImg":"images/pic1.jpg"},{"recomImg":"images/pic2.jpg"},{"recomImg":"images/pic3.jpg"},{"recomImg":"images/pic4.jpg"},{"recomImg":"images/pic5.jpg"},{"recomImg":"images/pic6.jpg"},{"recomImg":"images/pic7.jpg"},{"recomImg":"images/pic8.jpg"},{"recomImg":"images/pic9.jpg"},{"recomImg":"images/pic10.jpg"},{"recomImg":"images/pic11.jpg"},{"recomImg":"images/pic12.jpg"},
 ]
